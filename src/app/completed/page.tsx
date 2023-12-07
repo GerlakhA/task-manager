13c7894b2171eb424150cd44a734267235a2fb13
Andrey Gerlakh <andrey@MacBook-Air-Andrey.local>
2023-12-08 01:37:39 +0300

    fixed completed and important page

--- src/app/completed/page.tsx
+++ src/app/completed/page.tsx
@@ -1,12 +1,63 @@
 'use client'
 
+import TaskItem from '@/components/Tasks/TaskItem'
+import { IGetTasks } from '@/types/IGetTasks'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import { BeatLoader } from 'react-spinners'
+
 const page = () => {
+	const client = useQueryClient()
+
+	const tasks = useQuery({
+		queryKey: ['get allTasks'],
+		queryFn: async () => {
+			const res = await axios.get<IGetTasks[]>(`/api/tasks`)
+			return res.data
+		},
+	})
+
+	const filter = tasks?.data?.filter(item => item.isCompleted === true)
+
+	// console.log(filter)
+
+	const deleteTasks = useMutation({
+		mutationKey: ['delete item'],
+		mutationFn: async (id: string) => {
+			await axios.delete(`/api/tasks/${id}`)
+		},
+	})
+	client.invalidateQueries({ queryKey: ['get important'] })
+	toast.success('Task delete successfully!')
+
+	if (tasks.isLoading) return <BeatLoader color='#36d7b7' />
+
+	const deleteTaskById = (id: string) => {
+		deleteTasks.mutate(id)
+	}
+
+	// const important = async () => {
+	// 	'use server'
+	// 	const { isImportant } = await prisma.task.fields
+
+	// 	return isImportant ? true : false
+	// }
+
 	return (
-		<div className='flex justify-start items-center '>
-			<div className='border-b-4 border-green-500'>
-				<h1 className='text-4xl font-semibold'>Completed Tasks</h1>
-			</div>
-			<div></div>
+		<div className='w-full h-full overflow-y-auto'>
+			<h1 className='text-4xl font-semibold border-b-4 border-green-500 w-[250px]'>
+				Completed tasks!
+			</h1>
+			{filter?.map(item => (
+				<div
+					key={item.id}
+					className='flex justify-start items-center w-[300px] h-[200px] border border-green-500
+					mt-10 rounded-xl bg-neutral-600'
+				>
+					<TaskItem data={item} />
+				</div>
+			))}
 		</div>
 	)
 }
--- src/app/important/page.tsx
+++ src/app/important/page.tsx
@@ -4,47 +4,32 @@ import TaskItem from '@/components/Tasks/TaskItem'
 import { IGetTasks } from '@/types/IGetTasks'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
-import toast from 'react-hot-toast'
 import { BeatLoader } from 'react-spinners'
 
 const page = () => {
 	const client = useQueryClient()
 
 	const tasks = useQuery({
-		queryKey: ['get important'],
+		queryKey: ['get allTasks'],
 		queryFn: async () => {
 			const res = await axios.get<IGetTasks[]>(`/api/tasks`)
 			return res.data
 		},
 	})
 
-	const filter = tasks?.data?.filter(item => item.important === true)
+	const filter = tasks?.data?.filter(item => item.isImportant === true)
 
-	console.log(filter)
+	// console.log(filter)
 
 	const deleteTasks = useMutation({
 		mutationKey: ['delete item'],
 		mutationFn: async (id: string) => {
 			await axios.delete(`/api/tasks/${id}`)
 		},
-		onSuccess: () => {
-			client.invalidateQueries({ queryKey: ['get important'] })
-			toast.success('Товар успешно удален из корзины!')
-		},
 	})
+	// client.invalidateQueries({ queryKey: ['get important'] })
 
 	if (tasks.isLoading) return <BeatLoader color='#36d7b7' />
-
-	const deleteTaskById = (id: string) => {
-		deleteTasks.mutate(id)
-	}
-
-	// const important = async () => {
-	// 	'use server'
-	// 	const { isImportant } = await prisma.task.fields
-
-	// 	return isImportant ? true : false
-	// }
 
 	return (
 		<div className='w-full h-full overflow-y-auto'>
